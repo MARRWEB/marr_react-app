f475a399ca579ac31ef9fb2e10ce511a43b333e0
jyjyjy25 <jyjyjy7418@gmail.com>
2023-06-15 22:17:09 +0900

    Add: SearchBar component

---
+++ marr/src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const SearchBar = () => {
+  const [value, setValue] = useState("");
+  return (
+    <div className="SearchBar">
+      <div className="search-wrapper">
+        <input
+          className="input-search"
+          type="text"
+          maxLength="50"
+          placeholder="지역, 음식점 입력"
+          autoComplete="off"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button className="btn-search" type="button" alt="제출버튼">
+          <img src={process.env.PUBLIC_URL + `assets/search_icon.png`} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- marr/src/pages/Home.js
+++ marr/src/pages/Home.js
@@ -1,7 +1,13 @@
 import MyHeader from "../components/MyHeader";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
-  return <MyHeader />;
+  return (
+    <div>
+      <MyHeader />
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Home;

50c9a3c7b5b673a0e3aaf606d1ae7da8ec6d395d
jyjyjy25 <jyjyjy7418@gmail.com>
2023-06-16 00:56:57 +0900

    Add: Map component with kakao map api

---
+++ marr/src/components/Map.js
@@ -0,0 +1,65 @@
+import { useRef, useMemo, useState, useEffect } from "react";
+
+const { kakao } = window;
+
+const Map = () => {
+  const mapContainer = useRef(null);
+  const [location, setLocation] = useState({ latitude: "", longitude: "" });
+
+  const MapOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  // 1) 현재 위치 가져오기
+  const getCurrLocation = useMemo(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error, MapOptions);
+    }
+
+    function success(position) {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    }
+
+    function error() {
+      setLocation({
+        latitude: 37.483034,
+        longitude: 126.902435,
+      });
+    }
+  }, [navigator.geolocation.getCurrentPosition]);
+
+  // 2) 카카오맵 불러오기
+  const kakaoMap = () => {
+    const options = {
+      center: new kakao.maps.LatLng(location.latitude, location.longitude),
+      level: 3,
+    };
+    const map = new window.kakao.maps.Map(mapContainer.current, options);
+    const marker = new window.kakao.maps.Marker({
+      position: options.center,
+    });
+
+    marker.setMap(map);
+  };
+
+  useEffect(() => {
+    kakao.maps.load(() => kakaoMap());
+  }, [location]);
+
+  return (
+    <div className="Map">
+      <div
+        id="map"
+        ref={mapContainer}
+        style={{ width: "100%", height: "400px" }}
+      ></div>
+    </div>
+  );
+};
+
+export default Map;
--- marr/src/pages/Home.js
+++ marr/src/pages/Home.js
@@ -1,11 +1,13 @@
 import MyHeader from "../components/MyHeader";
 import SearchBar from "../components/SearchBar";
+import Map from "../components/Map";
 
 const Home = () => {
   return (
     <div>
       <MyHeader />
       <SearchBar />
+      <Map />
     </div>
   );
 };

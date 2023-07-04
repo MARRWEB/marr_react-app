b1c877f74c4c7c31d305f1cbf8681d6ae7f11c73
sooochan <nowee018@naver.com>
2023-07-04 18:39:38 +0900

    add : HOME

--- src/pages/GoogleButton.js
+++ src/pages/GoogleButton.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import MyHeader from './../components/MyHeader';
+
 
 const GoogleButton = () => {
   const clientId = '1059585472363-vn4ejudkdn6fkncnnebcjoiga8bh17bt.apps.googleusercontent.com';
@@ -22,12 +24,15 @@ const GoogleButton = () => {
 
   return (
     <>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={handleSuccess}
-          onFailure={handleFailure}
-        />
-      </GoogleOAuthProvider>
+      <MyHeader headText={"당신의 MARR"} />
+      <div className="Home">
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            onSuccess={handleSuccess}
+            onFailure={handleFailure}
+          />
+        </GoogleOAuthProvider>
+      </div>
     </>
   );
 };
